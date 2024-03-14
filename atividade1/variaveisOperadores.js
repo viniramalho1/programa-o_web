e28e63e5814d5856944ae0e6c0bb86e11cfc2437
VINÍCIUS RAMALHO DE ALMEIDA <vinicius.almeida@a.ucb.br>
2024-03-14 10:22:18 -0300

    Adiciona solução da lista 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+const a = 70
+const b = 95
+ 
+c = a + b
+
+console.log(`Resultado da soma entre ${a} e ${b} é ${c}`)
+
+c = a - b
+
+console.log(`Resultado da subtração entre ${a} e ${c}`)
+
+c = a / b
+
+console.log(`Resultado da divisão entre ${a} e ${b} e ${c}`)
+
+c = a * b
+
+console.log(`Resultado da multiplicação entre ${a} e ${b} e ${c}`)
