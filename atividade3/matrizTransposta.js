5b85dbca45034c2f1fc6ce37c660335054c95360
Vinicius <vralmeida6@gmail.com>
2024-03-28 23:03:51 -0300

    Atividade3 Finalizada

--- atividade3/matrizTransposta.js
+++ atividade3/matrizTransposta.js
@@ -1,24 +1,29 @@
-// Matriz original
+
+function transporMatriz(matriz){
+
+    const linhas = matriz.length;
+    const colunas = matriz[0].length;
+
+    const matriz_transposta = [];
+
+    // Iterar sobre as colunas da matriz original
+    for (let j = 0; j < colunas; j++) {
+        matriz_transposta[j] = [];
+        for (let i = 0; i < linhas; i++) {
+            matriz_transposta[j][i] = matriz[i][j];
+        }
+    }
+
+    // Imprimir a matriz transposta
+    for (let i = 0; i < matriz_transposta.length; i++) {
+        console.log(matriz_transposta[i].join(" "));
+    }
+}
+
 const matriz = [
     [1, 2],
     [3, 4],
     [5, 6]
 ];
 
-const linhas = matriz.length;
-const colunas = matriz[0].length;
-
-const matriz_transposta = [];
-
-// Iterar sobre as colunas da matriz original
-for (let j = 0; j < colunas; j++) {
-    matriz_transposta[j] = [];
-    for (let i = 0; i < linhas; i++) {
-        matriz_transposta[j][i] = matriz[i][j];
-    }
-}
-
-// Imprimir a matriz transposta
-for (let i = 0; i < matriz_transposta.length; i++) {
-    console.log(matriz_transposta[i].join(" "));
-}
+transporMatriz(matriz)
